a2836a853ae3635234b2581793cc0891ae046ffb
AllocatedArtist <59222318+AllocatedArtist@users.noreply.github.com>
2026-09-11 00:27:07 -0700

    camera component has depth and render mode for sorting/render target updates

--- SynoraEngine/include/SynoraEngine/scene/components/CameraComponent.h
+++ SynoraEngine/include/SynoraEngine/scene/components/CameraComponent.h
@@ -2,11 +2,17 @@
 
 namespace SYN {
 struct CameraComponent {
+    enum class RenderMode { Continuous, OnDemand };
+
     float fovDegrees = 90.f;
     float aspectRatio = 16.f / 9.f;
     float nearPlane = 0.1;
     float farPlane = 100.f;
     bool isPrimary;
+
+    uint32_t depth = 0;
+    RenderMode renderMode = RenderMode::Continuous;
+    bool dirty = false;
 };
 
 // Attach with CameraComponent
